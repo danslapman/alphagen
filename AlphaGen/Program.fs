f10230543a48a17fce262fee5e0c5a325110b2d9
Daniil Smirnov <danslapman@gmail.com>
2014-11-27 20:00:39 +0300

    Character duplication check

--- AlphaGen/Program.fs
+++ AlphaGen/Program.fs
@@ -1,33 +1,31 @@
 ﻿open Symbols
 open System
 
-let makeCharSeq (characters: char seq) elLen =
+let checkCharDoubling factor (st: char seq) =
+    let freq = Seq.fold (fun (m: Map<char, int>) i -> if m.ContainsKey(i) then m.Add (i, m.Item(i) + 1) else m.Add(i, 1)) Map.empty st
+    Map.forall (fun k v -> v <= factor) freq
+
+let makeCharSeq (characters: char seq) elLen  =
     let computeVariants (st: char seq seq) =
-        characters |> Seq.map (fun ch -> Seq.map (fun s -> (Seq.append (seq[ch]) s)) st) |> Seq.concat
+        //characters |> Seq.map (fun ch -> Seq.map (fun s -> Seq.append (Seq.singleton ch) s) st) |> Seq.concat
+        seq {
+            for ch in characters do
+                for sqns in st do
+                    yield Seq.append (Seq.singleton ch) sqns
+        }
         
     let rec makeSeqLevel (subSeq: char seq seq) currentLevel = 
         if currentLevel = 0 then
             subSeq
         else
             makeSeqLevel (computeVariants subSeq) (currentLevel - 1)
-    makeSeqLevel [[]] elLen
-(*
-let makeCharList (characters: char list) elLen =
-    let computeVariants (st: char list list) =
-        characters |> List.map (fun ch -> List.map (fun s -> ch :: s) st) |> List.concat
-        
-    let rec makeSeqLevel (subSeq: char list list) currentLevel = 
-        if currentLevel = 0 then
-            subSeq
-        else
-            makeSeqLevel (computeVariants subSeq) (currentLevel - 1)
-    makeSeqLevel [[]] elLen
-*)
+    makeSeqLevel [[]] elLen |> Seq.filter (checkCharDoubling 3)
 
 [<EntryPoint>]
 let main argv = 
-    let of3 = makeCharSeq AllSymbols 8 |> Seq.map System.String.Concat
-    printfn "%A" of3
+    let of3 = makeCharSeq AllSymbols 10 |> Seq.map System.String.Concat
+    for l in of3 do
+        printfn "%s" l
 
     Console.ReadKey() |> ignore
     0
